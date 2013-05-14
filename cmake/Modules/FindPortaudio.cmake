2d5f3cea3a370344a209ee5c65e985bcafd3c775
Diane Bruce <db@db.net>
2013-05-14 02:35:51 -0700

    cmake: Change for FreeBSD.

--- cmake/Modules/FindPortaudio.cmake
+++ cmake/Modules/FindPortaudio.cmake
@@ -6,7 +6,13 @@
 #  PORTAUDIO_LIBRARIES - Link these to use Portaudio
 
 include(FindPkgConfig)
-pkg_check_modules(PC_PORTAUDIO portaudio)
+if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+  message( "--- Using Portaudio2 for FreeBSD")
+  set(PORTAUDIO_FOUND TRUE)
+  set(PORTAUDIO_INCLUDE_DIRS "/usr/local/include/portaudio2")
+  set(PORTAUDIO_LIBRARIES "/usr/local/lib/portaudio2/libportaudio.so")
+else()
+  pkg_check_modules(PC_PORTAUDIO portaudio)
 
 find_path(PORTAUDIO_INCLUDE_DIRS
   NAMES
@@ -28,7 +34,7 @@ find_library(PORTAUDIO_LIBRARIES
   HINTS
     ${PC_PORTAUDIO_LIBDIR}
 )
-
+endif()
 mark_as_advanced(PORTAUDIO_INCLUDE_DIRS PORTAUDIO_LIBRARIES)
 
 INCLUDE(FindPackageHandleStandardArgs)
--- cmake/Modules/FindUSB.cmake
+++ cmake/Modules/FindUSB.cmake
@@ -1,20 +1,34 @@
 if(NOT LIBUSB_FOUND)
-  pkg_check_modules (LIBUSB_PKG libusb-1.0)
-  find_path(LIBUSB_INCLUDE_DIR NAMES libusb.h
+    pkg_check_modules (LIBUSB_PKG libusb-1.0)
+
+    find_path(LIBUSB_INCLUDE_DIR NAMES libusb.h
     PATHS
     ${LIBUSB_PKG_INCLUDE_DIRS}
     /usr/include/libusb-1.0
     /usr/include
-    /usr/local/include
   )
 
-  find_library(LIBUSB_LIBRARIES NAMES usb-1.0
+  if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+    message(status " Looking for FreeBSD usb")
+    find_library(LIBUSB_LIBRARIES NAMES usb-1.0
+    PATHS
+    ${LIBUSB_PKG_LIBRARY_DIRS}
+    /usr/lib
+  )
+  elseif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+    find_library(LIBUSB_LIBRARIES NAMES usb-1.0
+    PATHS
+    ${LIBUSB_PKG_LIBRARY_DIRS}
+    /usr/lib
+  )
+  endif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+
+  find_library(LIBUSB_LIBRARIES NAMES usb
     PATHS
     ${LIBUSB_PKG_LIBRARY_DIRS}
     /usr/lib
     /usr/local/lib
   )
-
 if(LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
   set(LIBUSB_FOUND TRUE CACHE INTERNAL "libusb-1.0 found")
   message(STATUS "Found libusb-1.0: ${LIBUSB_INCLUDE_DIR}, ${LIBUSB_LIBRARIES}")
